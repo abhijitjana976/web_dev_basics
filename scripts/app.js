52f7991b3a7849facd3d7290fd7b0ca26b94746b
saikat180891 <saptadipa123@gmail.com>
2021-05-14 17:58:10 +0530

    Added ToDo Basics

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const addButton = document.querySelector("#add"); // HTML Element -> object -> addEventlistener
+console.log(addButton)
+addButton.addEventListener("click", function () {
+    alert("hello")
+});
