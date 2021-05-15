80f59745f8fa84988dede5450fdb4582948a8b9f
saikat180891 <saptadipa123@gmail.com>
2021-05-15 18:09:16 +0530

    Added list

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 const addButton = document.querySelector("#add"); // HTML Element -> object -> addEventlistener
-console.log(addButton)
+const list = document.querySelector("#list");
+
 addButton.addEventListener("click", function () {
-    alert("hello")
+    const inputBox = document.querySelector("#text-input");
+    const li = document.createElement("li");
+    li.textContent = inputBox.value;
+    li.classList.add("list-content");
+    li.style.border = "1px solid black";
+    list.append(li);
+    inputBox.value = "";
 });
